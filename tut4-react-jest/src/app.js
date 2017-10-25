478b0e5e4016d2cb2d7ffb59dd96cf8f43659a6c
samarjit Samanta <samarjit.samanta@bangalore.accenture.bls.com>
2017-10-25 15:47:13 +0530

    updated folder structure

--- tut4-react-jest/src/app.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Clock from './app/Clock'
-import CountdownForm from './app/Countdown.form'
-
-export default class App extends React.Component {
-  render () {
-    return <div> Hello React! <Clock/> <CountdownForm/> </div>;
-  }
-}
